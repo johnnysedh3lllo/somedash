768281b74f6e45a0b82bd73bab949b7df14c3f0f
johnnysedh3lllo <johnnysaidh3lllo@gmail.com>
2025-02-04 17:30:01 +0100

    added slight test for mobile

--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -2,6 +2,7 @@
 
 const toggleBtn = document.querySelector(".theme-toggle__btn");
 const toggleSwitch = document.querySelector(".theme-toggle__btn__switch");
+const test = document.querySelector(".test");
 
 const colorScheme = window.matchMedia("(prefers-color-scheme: light)");
 
@@ -16,6 +17,10 @@ colorScheme.addEventListener("change", (e) => {
 
 let isClicked = false;
 toggleBtn?.addEventListener("click", () => {
+  test.textContent = "working";
+  setTimeout(() => {
+    test.textContent = "";
+  }, 1000);
   isClicked = !isClicked;
   toggleTheme(!isClicked);
 });
