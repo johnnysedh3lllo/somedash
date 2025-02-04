024eb19be055fd2a4373235aab90243ff97b078c
johnnysedh3lllo <johnnysaidh3lllo@gmail.com>
2025-02-04 18:14:46 +0100

    renamed classes and values to improve readability

--- src/scripts/toggle.ts
+++ src/scripts/toggle.ts
@@ -2,25 +2,28 @@
 
 const toggleBtn = document.querySelector(".theme-toggle__btn");
 const toggleSwitch = document.querySelector(".theme-toggle__btn__switch");
-const test = document.querySelector(".test");
 
 const colorScheme = window.matchMedia("(prefers-color-scheme: light)");
 
-const toggleTheme = (match) => {
-  toggleSwitch?.setAttribute("data-toggle", match ? "false" : "true");
+const toggleTheme = (match: boolean) => {
+  toggleSwitch?.setAttribute("data-theme", match ? "light" : "dark");
 };
 
-colorScheme.addEventListener("change", (e) => {
+colorScheme.addEventListener("change", (e: MediaQueryListEvent) => {
   const isLight = e.matches;
   toggleTheme(isLight);
 });
 
-let isClicked = false;
+let isClicked: boolean = false;
 toggleBtn?.addEventListener("click", () => {
-  test.textContent = "working";
-  setTimeout(() => {
-    test.textContent = "";
-  }, 1000);
   isClicked = !isClicked;
   toggleTheme(!isClicked);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (colorScheme.matches) {
+    toggleTheme(true);
+  } else {
+    toggleTheme(false);
+  }
 });
